ad3f71f7b3ef8765ed483d67b47d9dc182d647f6
CheonMinJae <cmjcmj1011@gmail.com>
2024-09-12 16:33:43 +0900

    chore:spotless setting

--- src/main/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplication.java
+++ src/main/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectBareGroundHeadingApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectBareGroundHeadingApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(ProjectBareGroundHeadingApplication.class, args);
+  }
 }
+
+/** TODO: 기본적인 세팅하기 1. spotless 세팅 2. 도커 세팅 3. 깃헙 액션으로 pre-commit 세팅하기 a. pre-commit b. 디코에 연결하기 */
