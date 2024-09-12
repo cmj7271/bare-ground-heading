364149e59bdbb3d5e74d68b4bde2795a5af8e8e1
CheonMinJae <cmjcmj1011@gmail.com>
2024-09-12 15:09:10 +0900

    initial setting

---
+++ src/main/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplication.java
@@ -0,0 +1,13 @@
+package com.gdsc.project_bare_ground_heading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBareGroundHeadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBareGroundHeadingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdsc.project_bare_ground_heading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBareGroundHeadingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
