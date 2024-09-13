4af491854bcd5922cba03159da019dd0753c5f5d
CheonMinJae <cmjcmj1011@gmail.com>
2024-09-13 15:33:40 +0900

    docker compose setting and test api

--- src/main/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplication.java
+++ src/main/java/com/gdsc/project_bare_ground_heading/ProjectBareGroundHeadingApplication.java
@@ -2,9 +2,18 @@ package com.gdsc.project_bare_ground_heading;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProjectBareGroundHeadingApplication {
+
+  // test api
+  @RequestMapping("/")
+  public String home() {
+    return "Hello World";
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ProjectBareGroundHeadingApplication.class, args);
